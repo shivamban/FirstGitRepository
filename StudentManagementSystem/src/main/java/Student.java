23a7f656f50f658e4a138135c9428aae37809ca1
shivambanyal <shivam.banyal@kristasoft.com>
2023-07-25 01:36:58 +0530

    first commit

--- StudentManagementSystem/src/main/java/Student.java
+++ StudentManagementSystem/src/main/java/Student.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private final String name;
+    private final int age;
+    private final int rollNumber;
+
+
+    public Student(String name, int age, int rollNumber) {
+        this.name = name;
+        this.age = age;
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+
 }
